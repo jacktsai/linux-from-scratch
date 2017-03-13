42c7efb5be02c57ed66a91822d7097be25ec6532
Jack Tsai <wa0327@gmail.com>
2017-03-13 16:37:01 +0800

    update

---
+++ scripts/install-lfs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+if [ -e /etc/lfs-release ]; then
+    [ $(id -un) != 'root' ] && {
+        echo '在 LFS 環境請使用 root 使用者執行.'
+        exit 1
+    }
+else
+    [ $(id -un) != 'lfs' ] && {
+        echo '在非 LFS 環境請使用 lfs 使用者執行.'
+        exit 1
+    }
+fi
+
+
+SH_DIR=$(dirname $BASH_SOURCE[0])
+mkdir /install_log 2> /dev/null
+
+function install_pkg() {
+    echo installing $1 ...
+    if [ -e /install_log/$1 ]; then
+        return 0
+    fi
+
+    SH_DIR/install.sh SH_DIR/03.lfs/$1 && return 0 || return 1
+}
+
+install_pkg linux_headers && \
+install_pkg man_pages && \
+install_pkg glibc
