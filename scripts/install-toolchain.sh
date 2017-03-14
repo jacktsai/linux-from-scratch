1b266fbaedccc393e4311d2b6c19acfae0908ca7
Jack Tsai <wa0327@gmail.com>
2017-03-14 14:39:03 +0800

    update

--- scripts/install-toolchain.sh
+++ scripts/install-toolchain.sh
@@ -13,12 +13,12 @@ SCRIPT_DIR=$(dirname $BASH_SOURCE[0])
 mkdir /tools/install-log 2> /dev/null
 
 function install_pkg() {
-    echo Installing $1 ...
     [ -e /tools/install-log/$1 ] && {
         echo $1 already installed.
         return 0
     }
 
+    printf '\nInstalling %s ...\n' $1
     $SCRIPT_DIR/install.sh $SCRIPT_DIR/01.toolchain/$1 && {
         touch /tools/install-log/$1
         return 0
@@ -32,5 +32,17 @@ install_pkg binutils-cross \
 && install_pkg libstdc++ \
 && install_pkg binutils \
 && install_pkg gcc \
-&& install_pkg tcl-core
-
+&& install_pkg tcl-core \
+&& install_pkg expect \
+&& install_pkg dejagu \
+&& install_pkg check \
+&& install_pkg ncurses \
+&& install_pkg bash \
+&& install_pkg bison \
+&& install_pkg bzip2 \
+&& install_pkg coreutils \
+&& install_pkg diffutils \
+&& install_pkg file \
+&& install_pkg findutils \
+&& install_pkg gawk \
+&& install_pkg gettext
