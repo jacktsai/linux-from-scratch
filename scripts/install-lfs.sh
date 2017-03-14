e941c40a3225ff9fedd02dc96904ffcb222dedb5
Jack Tsai <wa0327@gmail.com>
2017-03-14 15:35:48 +0800

    update

--- scripts/install-lfs.sh
+++ scripts/install-lfs.sh
@@ -11,19 +11,22 @@ else
     }
 fi
 
-
-SH_DIR=$(dirname $BASH_SOURCE[0])
-mkdir /install_log 2> /dev/null
+SCRIPT_DIR=$(dirname $BASH_SOURCE[0])
+mkdir /install-log 2> /dev/null
 
 function install_pkg() {
-    echo installing $1 ...
-    if [ -e /install_log/$1 ]; then
+    [ -e /install-log/$1 ] && {
+        echo $1 already installed.
         return 0
-    fi
+    }
 
-    SH_DIR/install.sh SH_DIR/03.lfs/$1 && return 0 || return 1
+    printf '\nInstalling %s ...\n' $1
+    $SCRIPT_DIR/install.sh $SCRIPT_DIR/03.toolchain/$1 && {
+        touch /install-log/$1
+        return 0
+    } || return 1
 }
 
-install_pkg linux_headers && \
-install_pkg man_pages && \
-install_pkg glibc
+linux_headers \
+&& install_pkg man_pages \
+&& install_pkg glibc
